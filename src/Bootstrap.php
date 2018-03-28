974c62ef3a07922acb69cd08f55c714889b71511
Edgar Ochoa <eochoa3@ucol.mx>
2018-03-27 22:12:52 -0600

    Error Handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,21 @@
 <?php declare(strict_types = 1);
 
-echo 'Hello World';
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register de error handler
+ */
+
+ $whoops = new \Whoops\Run;
+if($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
